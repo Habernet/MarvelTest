4e7660b6157c4e64c89d047956633d815ec85817
habernet <phaberern@gmail.com>
2019-04-30 20:53:54 -0400

    Lots of stuff I forgot 5

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -16,35 +16,19 @@ $(document).ready(function () {
         var cardbody = $("<div>").addClass("card-body");
         // create a p with class card-text and set the text to cardDescription
         var paragraph = $("<p>").addClass("card-text").text(cardDescription);
-               // create a button with class winner and create a data-name and set that equal to cardName
+        // create a button with class winner and create a data-name and set that equal to cardName
         var btn = $("<button>").addClass("winner").attr("data-name", cardName);
 
         cardbody.append(paragraph);
         card.append(header, img, cardbody);
         $("#character-cards").append(card);
-
-
-
-
-        // <div class="card">
-        //   <h5 class="card-header text-center">
-        //     HULK
-        //   </h5>
-        //   <img class="card-img-top" src="https://media.giphy.com/media/aS8ypUweGOXMA/giphy.gif" alt="Card image cap">
-        //   <div class="card-body">
-        //     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-        //       content.</p>
-        //   </div>
-        //   <div class="card-body">
-        //   <h5>Battle creds here!</h5>
-        //   </div>
-        // </div>
-    }
+        // Add Battle Creds!
+    };
 
     // List of characters we know have descriptions in Marvel.
     var characters = [
         "Spider-Man", "Thor", "Hulk", "Wolverine", "Thanos",
-        "Loki", "Magneto", "Cyclops", "Deadpool", "Iron-Man", "Captain-America"
+        "Loki", "Magneto", "Cyclops", "Deadpool", "Captain-America"
     ];
     // Marvel Key
     const marvelKey = "3825528115714235769b996819f21ef0";
@@ -69,70 +53,60 @@ $(document).ready(function () {
     $("#submit").on("click", (e) => {
         e.preventDefault();
 
-        //If numOfChars < 2
+        // While numOfChars < 2
         //      do the stuff
         //      at end of stuff increment numOfChars by 1
         // Choose a random character from the pool that we know have descriptions.
-        var charIndex = Math.floor(Math.random() * 11);
-        var chartoSearch = characters[charIndex];
-        console.log(chartoSearch);
+        while (numOfChars < 2) {
+            var charIndex = Math.floor(Math.random() * 10);
+            var chartoSearch = characters[charIndex];
+            console.log(chartoSearch);
 
-        // Check firebase to see if character exists
-        database.ref("characters/").orderByChild("name").equalTo(chartoSearch).once("value", snapshot => {
-            if (snapshot.exists()) {
-                var snap = snapshot.val(); // Why doesn't this work??
-                snap = snap[Object.keys(snap)[0]]
-                console.log("IT WORKED! ",  snap);
-                createCharCard(snap);
-                // Call createCharCard and pass it the character node in firebase
-
-
-            } else {
-                // Create the character based on calls
-
-                // Marvel Query Built
-                var marvelQuery = "https://gateway.marvel.com:443/v1/public/characters?name=" + chartoSearch + "&limit=2&apikey=" + marvelKey;
-                // Giphy Query Built
-                var giphyQuery = "https://api.giphy.com/v1/gifs/search?q=" + chartoSearch + "&limit=1&api_key=" + giphyKey;
-
-                $.ajax({
-                    url: marvelQuery,
-                    method: "GET"
-                }).then(response => {
-                    console.log(response);
-                    // Store description in variable
-                    var description = response.data.results[0].description;
+            // Check firebase to see if character exists
+            database.ref("characters/").orderByChild("name").equalTo(chartoSearch).once("value", snapshot => {
+                if (snapshot.exists()) {
+                    var snap = snapshot.val();
+                    snap = snap[Object.keys(snap)[0]]
+                    createCharCard(snap);
+                } else {
+                    // Create the character based on calls
+                    // Marvel Query 
+                    var marvelQuery = "https://gateway.marvel.com:443/v1/public/characters?name=" + chartoSearch + "&limit=2&apikey=" + marvelKey;
+                    // Giphy Query 
+                    var giphyQuery = "https://api.giphy.com/v1/gifs/search?q=" + chartoSearch + "&limit=1&api_key=" + giphyKey;
 
                     $.ajax({
-                        url: giphyQuery,
+                        url: marvelQuery,
                         method: "GET"
                     }).then(response => {
                         console.log(response);
-                        // Store gif url in variable
-                        var gifURL = response.data[0].images.fixed_height.url;
-                        // Initialize battlecred to 0
-                        var battleCred = 0;
+                        // Store description in variable
+                        var description = response.data.results[0].description;
 
-                        // Create object to push to database
-                        database.ref("characters/").push({
-                            description: description,
-                            battlecred: battleCred,
-                            name: chartoSearch,
-                            gif: gifURL
-                        });
-                        // Call createCharCard and pass it the character node in firebase
+                        $.ajax({
+                            url: giphyQuery,
+                            method: "GET"
+                        }).then(response => {
+                            console.log(response);
+                            // Store gif url in variable
+                            var gifURL = response.data[0].images.fixed_height.url;
+                            // Initialize battlecred to 0
+                            var battleCred = 0;
 
+                            // Create object to push to database
+                            database.ref("characters/").push({
+                                description: description,
+                                battlecred: battleCred,
+                                name: chartoSearch,
+                                gif: gifURL
+                            });
+                            createCharCard(snap);
+                        }).catch(err => console.log(err))
                     }).catch(err => console.log(err))
-                }).catch(err => console.log(err))
-            }
-        });
-    })
-
-    // Define on-click for class of cards, use data-name
-    $(".winner").on("click", (e) => {
-        // This card was chosen to win the battle!
-        // Increment the battlecred by one!
-        // Hide the other card, make a call to Marvel to post some comics they are in!?
-        // This can be used as a reset of the game on a timeout of like 20 seconds.
+                }
+            });
+            // increment by one
+            numOfChars++;
+        }
     })
 })
