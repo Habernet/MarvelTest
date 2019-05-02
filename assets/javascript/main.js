af11f95b8bcb515797687c0cbdbabc1248821a91
habernet <phaberern@gmail.com>
2019-05-02 13:25:35 -0400

    Testing on THIS

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -108,7 +108,8 @@ $(document).ready(function () {
     $("body").on("click", ".winner", (e) => {
         e.preventDefault();
         // Change the background of the winner card? Maybe?
-        var nameToUpdate = e.attr("data-name");
+        console.log(e);
+        // var nameToUpdate = e.attr("data-name");
         console.log("click worked!", nameToUpdate);
         // use the data name of THIS to go into firebase and update their battle creds
         database.ref("characters/").orderByChild("name").equalTo(chartoSearch).once("value", snapshot =>{
