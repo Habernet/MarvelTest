186928d0400c29d5a4b4e8cde58a31edaaf8e89f
habernet <phaberern@gmail.com>
2019-05-02 10:08:56 -0400

    Trying to refactor, added button and battle creds

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -6,30 +6,77 @@ $(document).ready(function () {
         var cardDescription = snapshot.description;
         var cardBattleCreds = snapshot.battlecred;
         var cardIMG = snapshot.gif;
-        // create a div class card
         var card = $("<div>").addClass("card");
-        // create an h5 class card-header text-center...set the text to cardName
         var header = $("<h5>").addClass("card-header text-center").text(cardName);
-        // create an img class card-img-top and set the src to cardIMG
         var img = $("<img>").addClass("card-img-top").attr("src", cardIMG);
-        // create div with class card-body
         var cardbody = $("<div>").addClass("card-body");
-        // create a p with class card-text and set the text to cardDescription
         var paragraph = $("<p>").addClass("card-text").text(cardDescription);
-        // create a button with class winner and create a data-name and set that equal to cardName
         var btn = $("<button>").addClass("winner").attr("data-name", cardName);
 
-        cardbody.append(paragraph);
+        cardbody.append(paragraph, creds, cardBattleCreds);
         card.append(header, img, cardbody);
         $("#character-cards").append(card);
         // Add Battle Creds!
     };
+
+    function gameLoop() {
+        var charIndex = Math.floor(Math.random() * 9);
+        var chartoSearch = characters[charIndex];
+        console.log(chartoSearch);
+
+        // Check firebase to see if character exists
+        database.ref("characters/").orderByChild("name").equalTo(chartoSearch).once("value", snapshot => {
+            if (snapshot.exists()) {
+                var snap = snapshot.val();
+                snap = snap[Object.keys(snap)[0]]
+                createCharCard(snap);
+            } else {
+                // Create the character based on calls
+                // Marvel Query 
+                var marvelQuery = "https://gateway.marvel.com:443/v1/public/characters?name=" + chartoSearch + "&limit=2&apikey=" + marvelKey;
+                // Giphy Query 
+                var giphyQuery = "https://api.giphy.com/v1/gifs/search?q=" + chartoSearch + "&limit=1&api_key=" + giphyKey;
+
+                $.ajax({
+                    url: marvelQuery,
+                    method: "GET"
+                }).then(response => {
+                    console.log(response);
+                    // Store description in variable
+                    var description = response.data.results[0].description;
+
+                    $.ajax({
+                        url: giphyQuery,
+                        method: "GET"
+                    }).then(response => {
+                        console.log(response);
+                        // Store gif url in variable
+                        var gifURL = response.data[0].images.fixed_height.url;
+                        // Initialize battlecred to 0
+                        var battleCred = 0;
+
+                        // Create object to push to database
+                        database.ref("characters/").push({
+                            description: description,
+                            battlecred: battleCred,
+                            name: chartoSearch,
+                            gif: gifURL
+                        });
+                        // Load the characters card based on the calls made
+                        createCharCard(snap);
+                    }).catch(err => console.log(err))
+                }).catch(err => console.log(err))
+            }
+        });
+        numOfChars++;
+    }
 
     // List of characters we know have descriptions in Marvel.
     var characters = [
         "Spider-Man", "Thor", "Hulk", "Wolverine", "Thanos",
         "Loki", "Magneto", "Cyclops", "Deadpool"
     ];
+
     // Marvel Key
     const marvelKey = "3825528115714235769b996819f21ef0";
     // Giphy Key
@@ -54,56 +101,10 @@ $(document).ready(function () {
         e.preventDefault();
 
         while (numOfChars < 2) {
-            var charIndex = Math.floor(Math.random() * 9);
-            var chartoSearch = characters[charIndex];
-            console.log(chartoSearch);
-
-            // Check firebase to see if character exists
-            database.ref("characters/").orderByChild("name").equalTo(chartoSearch).once("value", snapshot => {
-                if (snapshot.exists()) {
-                    var snap = snapshot.val();
-                    snap = snap[Object.keys(snap)[0]]
-                    createCharCard(snap);
-                } else {
-                    // Create the character based on calls
-                    // Marvel Query 
-                    var marvelQuery = "https://gateway.marvel.com:443/v1/public/characters?name=" + chartoSearch + "&limit=2&apikey=" + marvelKey;
-                    // Giphy Query 
-                    var giphyQuery = "https://api.giphy.com/v1/gifs/search?q=" + chartoSearch + "&limit=1&api_key=" + giphyKey;
-
-                    $.ajax({
-                        url: marvelQuery,
-                        method: "GET"
-                    }).then(response => {
-                        console.log(response);
-                        // Store description in variable
-                        var description = response.data.results[0].description;
-
-                        $.ajax({
-                            url: giphyQuery,
-                            method: "GET"
-                        }).then(response => {
-                            console.log(response);
-                            // Store gif url in variable
-                            var gifURL = response.data[0].images.fixed_height.url;
-                            // Initialize battlecred to 0
-                            var battleCred = 0;
-
-                            // Create object to push to database
-                            database.ref("characters/").push({
-                                description: description,
-                                battlecred: battleCred,
-                                name: chartoSearch,
-                                gif: gifURL
-                            });
-                            createCharCard(snap);
-                        }).catch(err => console.log(err))
-                    }).catch(err => console.log(err))
-                }
-            });
-            numOfChars++;
+            gameLoop();
         }
         // Hide the submit button
         $("start-fight").hide();
+
     })
 })
