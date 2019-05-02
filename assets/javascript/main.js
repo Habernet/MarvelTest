4f806e650a60ab9bae8ac64fd4e9ca6711deeb12
habernet <phaberern@gmail.com>
2019-05-02 12:41:51 -0400

    Testing on click

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -106,7 +106,7 @@ $(document).ready(function () {
     })
 
     // Define on click for the winner button
-    $(".winner").on("click", (e) => {
+    $(".card").on("click", ".winner", (e) => {
         e.preventDefault();
         // Change the background of the winner card? Maybe?
         var nameToUpdate = $(this).data-name;
